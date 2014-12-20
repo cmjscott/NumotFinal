8d10b39fe2eb7069cb62b91b981736b8605b5513
cmjscott <cmjscott@gmail.com>
2014-12-19 20:23:14 -0500

    Finished vehicle and simulation to now use a data vector return instead of individual values. Will not build.

--- NumericalMethodsFinal/NumericalMethodsFinal/main.cpp
+++ NumericalMethodsFinal/NumericalMethodsFinal/main.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 
-#ifdef _MSC_VER
-#define _CRT_SECURE_NO_WARNINGS
-#endif
+//#ifdef _MSC_VER
+//#define _CRT_SECURE_NO_WARNINGS
+//#endif
 
 
 
@@ -88,6 +88,121 @@ int main()
 	return 0;
 }
 
+
+std::vector<std::vector<double> > simulation4(Vehicle testVehicle, double _dt, double _timeToFullThrottle)
+{
+	std::vector<std::vector<double> > data;
+	std::vector<double> time, vel, acceleration, torque, rpm, dataHolder;
+	double throttle(0), zeroToSixtyTime(0);
+
+	const double timeToFullThrottle = _timeToFullThrottle;
+
+	//Sets initial values
+	time.push_back(_dt);
+	vel.push_back(0);
+	acceleration.push_back(0);
+	torque.push_back(0);
+	rpm.push_back(0);
+	//force.push_back(0);
+	
+
+	std::cout << "----------------------------------Simulation 4----------------------------------" << std::endl;
+
+	do
+	{
+		throttle = time.back()/timeToFullThrottle;
+		dataHolder = testVehicle.simulateNextTimestep(vel.back(), _dt, throttle);
+		
+		time.push_back(time.back() + _dt);
+		vel.push_back(dataHolder[0]);
+		acceleration.push_back(dataHolder[1]);
+		torque.push_back(dataHolder[2]);
+		rpm.push_back(dataHolder[3]);
+
+		if (vel.back() * MS_TO_MPH >= 60 && zeroToSixtyTime == 0)
+			zeroToSixtyTime = time.back();
+
+	} while (time.back() < timeToFullThrottle);
+
+	do
+	{ 
+		dataHolder = testVehicle.simulateNextTimestep(vel.back(), _dt, throttle);
+
+		time.push_back(time.back() + _dt);
+		vel.push_back(dataHolder[0]);
+		acceleration.push_back(dataHolder[1]);
+		torque.push_back(dataHolder[2]);
+		rpm.push_back(dataHolder[3]);
+
+		if (vel.back() * MS_TO_MPH >= 60 && zeroToSixtyTime == 0)
+			zeroToSixtyTime = time.back();
+
+	} while ((vel.back() - vel.rbegin()[1]) / _dt > .1); // keep calculating velocity until the acceleration is less than .01 (essentially at max velocity)
+
+	std::cout << std::endl << "Your maximum velocity was " << vel.back() << " m/s  (" << vel.back() * MS_TO_MPH << " mph)" << std::endl;
+	std::cout << "Time to reach maximum throttle position: " << timeToFullThrottle << " seconds." << std::endl;
+	std::cout << "Time to reach maximum velocity: " << time.back() << " seconds." << std::endl;
+	std::cout << "0 - 60 time: " << zeroToSixtyTime << " seconds." << std::endl;
+	std::cout << "--------------------------------------------------------------------------------" << std::endl << std::endl;
+
+	data.push_back(time);
+	data.push_back(vel);
+	data.push_back(acceleration);
+	data.push_back(torque);
+	data.push_back(rpm);
+	return data;
+}
+
+void functionalityDemonstration()
+{
+	/*NOTE: Using 2001 Subaru legacy outback wagon, LL bean eddition for default simulation
+	Cd = .32
+	A = 23.4 ft^2 = 2.1739 m^2
+	mass = 3715 lbs = 1685.1 kg
+	Drive force ~ 1500 N
+	Braking force ~ 7700 N
+	Gear Ratios:
+	1st 2.785
+	2nd 1.545
+	3rd 1.000
+	4th 0.697
+	Rev 2.272
+	Diff 4.11
+
+	Tires: 225/60R16 98H SL
+	wheelRadius = 0.33782 m
+	
+	RPM =    { 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000, 4400, 4800, 5200, 5600, 6000, 6400, 6800 };
+	torque = { 240 , 250 , 260 , 270 , 280 , 290 , 300 , 305 , 310 , 305 , 295 , 285 , 280 , 270 , 260  };
+	*/
+
+	std::vector<std::vector<double> > test4Data;
+	std::vector<double> revMap = { 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000, 4400, 4800, 5200, 5600, 6000, 6400, 6800 };
+	std::vector<double> torqueMap = { 240, 250, 260, 270, 280, 290, 300, 305, 310, 305, 295, 285, 280, 270, 260 };
+	std::vector<double> gearRatios = { 2.785, 1.545, 1, .697 };
+	double rho(1.2041), dt(.001), mass(1685.1), Cdrag(.32), driveForce(1700), frontArea(2.1739), brakingForce(7700), diffRatio(4.11), wheelRadius(.33782);
+
+	//create vehicle objects for each simulation
+	Vehicle testVehicle(mass, Cdrag, frontArea, gearRatios, diffRatio, wheelRadius, rho);
+	engine* testEngine = new engine(revMap, torqueMap);
+
+	testVehicle.attachEngine(testEngine);
+
+	//Run simulations
+	test4Data = simulation4(testVehicle, dt);
+	
+
+
+	//Output data for matlab
+	util::outputData(test4Data, "test_data");
+	//util::outputData(test2Data, "simulation_2_example_data");
+	//util::outputData(test3Data, "simulation_3_example_data");
+	//util::outputData(test4Data, "simulation_4_example_data");
+
+}
+
+//Depreciated
+/*
 std::vector<std::vector<double> > simulation1(Vehicle testVehicle, double _dt)
 {
 	std::vector<std::vector<double> > data;
@@ -165,113 +280,4 @@ std::vector<std::vector<double> > simulation3(Vehicle testVehicle, double _dt)
 	data.push_back(vel);
 	return data;
 }
-
-std::vector<std::vector<double> > simulation4(Vehicle testVehicle, double _dt, double _timeToFullThrottle)
-{
-	std::vector<std::vector<double> > data;
-	std::vector<double> time, vel, rpm, force, torque;
-	double throttle(0), zeroToSixtyTime(0);
-
-	const double timeToFullThrottle = _timeToFullThrottle;
-
-	//Sets initial values
-	time.push_back(_dt);
-	vel.push_back(0);
-	//rpm.push_back(0);
-	//force.push_back(0);
-	//torque.push_back(0);
-
-	std::cout << "----------------------------------Simulation 4----------------------------------" << std::endl;
-
-	do
-	{
-		throttle = time.back()/timeToFullThrottle;
-		vel.push_back(testVehicle.velocity(vel.back(), _dt, throttle));
-		time.push_back(time.back() + _dt);
-
-		if (vel.back() * MS_TO_MPH >= 60 && zeroToSixtyTime == 0)
-			zeroToSixtyTime = time.back();
-
-		//rpm.push_back(testVehicle.getRPM());
-		//force.push_back(testVehicle.engineDriveForce(throttle));
-		//torque.push_back(testVehicle.getTorque(throttle));
-	} while (time.back() < timeToFullThrottle);
-
-	do
-	{ 
-		vel.push_back(testVehicle.velocity(vel.back(), _dt, throttle));
-		time.push_back(time.back() + _dt);
-
-		if (vel.back() * MS_TO_MPH >= 60 && zeroToSixtyTime == 0)
-			zeroToSixtyTime = time.back();
-
-		//rpm.push_back(testVehicle.getRPM());
-		//force.push_back(testVehicle.engineDriveForce(throttle));
-		//torque.push_back(testVehicle.getTorque(throttle));
-	} while ((vel.back() - vel.rbegin()[1]) / _dt > .1); // keep calculating velocity until the acceleration is less than .01 (essentially at max velocity)
-
-	std::cout << std::endl << "Your maximum velocity was " << vel.back() << " m/s  (" << vel.back() * MS_TO_MPH << " mph)" << std::endl;
-	std::cout << "Time to reach maximum throttle position: " << timeToFullThrottle << " seconds." << std::endl;
-	std::cout << "Time to reach maximum velocity: " << time.back() << " seconds." << std::endl;
-	std::cout << "0 - 60 time: " << zeroToSixtyTime << " seconds." << std::endl;
-	std::cout << "--------------------------------------------------------------------------------" << std::endl << std::endl;
-
-	data.push_back(time);
-	data.push_back(vel);
-	//data.push_back(rpm);
-	//data.push_back(force);
-	//data.push_back(torque);
-	return data;
-}
-
-void functionalityDemonstration()
-{
-	/*NOTE: Using 2001 Subaru legacy outback wagon, LL bean eddition for default simulation
-	Cd = .32
-	A = 23.4 ft^2 = 2.1739 m^2
-	mass = 3715 lbs = 1685.1 kg
-	Drive force ~ 1500 N
-	Braking force ~ 7700 N
-	Gear Ratios:
-	1st 2.785
-	2nd 1.545
-	3rd 1.000
-	4th 0.697
-	Rev 2.272
-	Diff 4.11
-
-	Tires: 225/60R16 98H SL
-	wheelRadius = 0.33782 m
-	
-	RPM =    { 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000, 4400, 4800, 5200, 5600, 6000, 6400, 6800 };
-	torque = { 240 , 250 , 260 , 270 , 280 , 290 , 300 , 305 , 310 , 305 , 295 , 285 , 280 , 270 , 260  };
-	*/
-	std::vector<std::string> v;
-	std::vector<std::vector<double> > test4Data;
-	std::vector<double> revMap = { 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000, 4400, 4800, 5200, 5600, 6000, 6400, 6800 };
-	std::vector<double> torqueMap = { 240, 250, 260, 270, 280, 290, 300, 305, 310, 305, 295, 285, 280, 270, 260 };
-	std::vector<double> gearRatios = { 2.785, 1.545, 1, .697 };
-	double rho(1.2041), dt(.001), mass(1685.1), Cdrag(.32), driveForce(1700), frontArea(2.1739), brakingForce(7700), diffRatio(4.11), wheelRadius(.33782);
-
-	//create vehicle objects for each simulation
-	Vehicle testVehicle(mass, Cdrag, frontArea, gearRatios, diffRatio, wheelRadius, rho);
-	engine* testEngine = new engine(revMap, torqueMap);
-
-	testVehicle.attachEngine(testEngine);
-
-	//Run simulations
-	test4Data = simulation4(testVehicle, dt);
-	
-	//std::cout << revMap << std::endl;
-	//_getch();
-
-	//Output data for matlab
-	util::outputData(test4Data, "test_data");
-	//util::outputData(test2Data, "simulation_2_example_data");
-	//util::outputData(test3Data, "simulation_3_example_data");
-	//util::outputData(test4Data, "simulation_4_example_data");
-
-}
-
-void testCase();
-
+*/
